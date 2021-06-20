6d59cc8439b5c8d2818b529d1197b0a552246eb0
googrand <sharkanovnikita@gmail.com>
2021-06-20 17:01:06 +0600

    Created input event listeners

--- sparky-core/main.cpp
+++ sparky-core/main.cpp
@@ -1,20 +1,38 @@
 #include "src\graphics\window.h"
-
+#include <iostream>
 int main()
 {
 	using namespace sparky;
 	using namespace graphics;
 	Window window("Sparky!", 800, 600);
 	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 	while (!window.closed())
 	{
 		window.clear();
+		if (window.isKeyPressed(GLFW_KEY_A)) 
+		{
+			std::cout << "Pressed A" << std::endl;
+		}		
+		if (window.isMouseButtonPressed(GLFW_MOUSE_BUTTON_LEFT)) 
+		{
+			std::cout << "Pressed A" << std::endl;
+		}		
+		double x, y;
+		window.getMousePosition(x, y);
+		std::cout << x << ", " << y << std::endl;
+#if 1
 		glBegin(GL_TRIANGLES);
 		glVertex2f(-0.5f, -0.5f);
 		glVertex2f(0.0f, 0.5f);
 		glVertex2f(0.5f, -0.5f);
 		glEnd();
-;		window.update(); 
+#else
+		glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
+#endif
+		window.update(); 
 	}
 	return 0;
 }
